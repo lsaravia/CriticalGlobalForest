7b6d1aaa19577d108de8a8ad42db45e019bb399d
Leonardo Saravia <lsaravia@ungs.edu.ar>
2016-05-14 02:52:24 -0300

    BBL comments: added examples to 2nd order transitions, move paragraph from methods to intro

--- R/map_fun.r
+++ R/map_fun.r
@@ -26,12 +26,12 @@ aggregate_top_200_patch <-function()
 	df <-ldply(strsplit(a_files,"_"))[,c(2,3)]
 	names(df)<-c("region","subregion")
 	df$id <- 1:nrow(df)
+	df$year <- gsub(".*\\.A([0-9]{4}).*","\\1",a_files)
 	group_by(df,region,subregion) %>% do(res=aggregate_top_200_patch_aux(a_files,.))
 }
 
 aggregate_top_200_patch_aux<-function(a_files,df)
 {
-	r_area<-stack()
 	dini<-df$id[1]
 	dfin<-df$id[nrow(df)]
 	
@@ -40,9 +40,27 @@ aggregate_top_200_patch_aux<-function(a_files,df)
 		if(is.na(crs(s_area))) stop("CRS not defined for :",a_files[i])
 		s_area <- aggregate(s_area,fact=10,fun=max)
 		a_area <- reclassify(s_area,c(2,256,0))
-		r_area <- addLayer(r_area,a_area)
+#		r_area <- addLayer(r_area,a_area)
+		plot(a_area, legend=FALSE, axes=FALSE, box=FALSE,paste0(df$region,df$subregion," ",df$year))	
+		fn <-paste0(strsplit(a_files[i],".tif_Top_200")[[1]][1],"_Top_2.tif")
+		writeRaster(a_area,filename=fn,format='GTiff',datatype='INT1U',overwrite=TRUE)
+		
 	}
-	fn <-paste0(strsplit(a_files[i],".A2")[[1]][1],".Top_2.tif")
-	writeRaster(r_area,filename=fn,format='GTiff',datatype='INT1U',overwrite=TRUE)
-	plot(r_area)	
 }	
+
+
+gif_top_2_patch <-function()
+{
+	a_files <-list.files(pattern="^.*Percent_Tree_Cover_Top_2\\.tif$")
+	#mer<- vector("list",length(a_files))
+	df <-ldply(strsplit(a_files,"_"))[,c(2,3)]
+	names(df)<-c("region","subregion")
+	df$id <- 1:nrow(df)
+	df$year <- gsub(".*\\.A([0-9]{4}).*","\\1",a_files)
+	group_by(df,region,subregion) %>% do(res=gif_top_2_patch_aux(a_files,.))
+}
+
+gif_top_2_patch_aux<-function(a_files,df)
+{
+	
+}
