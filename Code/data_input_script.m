ee6cbfd948663a2a0ccd2ee3c81aa06be2cba264
Santiago Raul Doyle <santiago.doyle@gmail.com>
2018-11-19 02:12:58 -0300

    Correct version of MATLAB function for image analyses corresponding to published 2018 paper - corrected dir study areas definition

--- Code/data_input_script.m
+++ Code/data_input_script.m
@@ -6,7 +6,7 @@ pars_gcf_analyses.thrsh_density=unique([[10:5:40] [25:2.5:35]]);
 pars_gcf_analyses.top_n_rank_graphics = 200;
 pars_gcf_analyses.re_do_analysis = 0 ;
 pars_gcf_analyses.do_graphical_out = 1;
-pars_gcf_analyses.area_definition_dir = 'F:\Dropbox_UNGS\Dropbox\GlobalCriticalForest\other_data\Study_areas_definition';
+pars_gcf_analyses.area_definition_dir = '/home/Santiago/Dropbox_UNGS/Dropbox/GlobalCriticalForest/other_data/Study_areas_definition';
 % location of area definition files
 analysis_label = 'GCF_first_paper_sensitivity_analysis';
 pars_gcf_analyses.analysis_label = analysis_label;
